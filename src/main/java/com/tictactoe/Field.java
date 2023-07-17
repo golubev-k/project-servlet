772763e647b9123333fd84ece5f8a316f546799e
golubev-k <konstantin.golubev92@gmail.com>
2023-07-17 12:57:02 +0500

    Finished project

--- src/main/java/com/tictactoe/Field.java
+++ src/main/java/com/tictactoe/Field.java
@@ -53,10 +53,19 @@ public class Field {
 
         for (List<Integer> winPossibility : winPossibilities) {
             if (field.get(winPossibility.get(0)) == field.get(winPossibility.get(1))
+                    && field.get(winPossibility.get(0)) == field.get(winPossibility.get(2))
+                    && field.get(winPossibility.get(0)) != Sign.EMPTY) {
+                return field.get(winPossibility.get(0));
+            }
+        }
+
+        for (List<Integer> winPossibility : winPossibilities) {
+            if (field.get(winPossibility.get(0)) == field.get(winPossibility.get(1))
                 && field.get(winPossibility.get(0)) == field.get(winPossibility.get(2))) {
                 return field.get(winPossibility.get(0));
             }
         }
         return Sign.EMPTY;
     }
+
 }
